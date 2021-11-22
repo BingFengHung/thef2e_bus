a9e4a41957814baafc8a0d31713aebff062d2225
BingFengHung <joe94008@gmail.com>
2021-11-22 13:10:32 +0000

    主頁面加入 Header 與 Footer 元件

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import style from './Footer.module.css'
+
+const Footer = () => {
+	return (
+		<div className={style.container}>
+			Taiwan Bus
+		</div>
+	)
+}
+
+export default Footer;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import style from './Header.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBus } from '@fortawesome/free-solid-svg-icons';
+
+const Header = () => {
+	return (
+		<div className={style.container}>
+			<div className={style.logo}>
+				<FontAwesomeIcon className={style.icon} icon={faBus}/>
+				<p>Taiwan Bus</p>
+			</div>
+		</div>
+	)
+}
+
+export default Header;
